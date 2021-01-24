19ca076a2ecebb14e28dd86ef8aca40376e2c3c1
jancpkgaming23 <72095721+jancpkgaming23@users.noreply.github.com>
2021-01-24 13:48:17 +0700

    Create JSO.php

---
+++ tools/JSO.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JSO Generator ./K4GUR4 X ploit</title>
+	<link href="https://fonts.googleapis.com/css?family=Iceland|Shadows+Into+Lights&display=swap" rel="stylesheet"> 
+	<script type="text/javascript">
+		function copy_text() {
+			document.getElementById("pilih").select();
+			document.execCommand("copy");
+			alert("Text berhasil dicopy ");
+		}
+	</script>
+	<style>
+	body {
+background-image:url();
+background-color: black;
+background-size: cover;
+background-attachment: fixed; 
+text-align: center;
+		color:white;
+		font-family: 'Shadows Into Lights', cursive;
+	}
+	.head {
+		font-family: 'Shadows Into Lights', cursive;
+		text-shadow: 0px 0px black;
+	}
+	.textarea {
+		width: 200px;
+		height: 100px;
+		border-radius: 10px;
+		border-style: unset;
+	}
+	.btn {
+		border-radius: 3px;
+		background-color: white;
+		border-style: unset;
+		font-family: 'Shadows Into Lights', cursive;
+	}
+	.foot {
+		font-family: 'Shadows Into Lights', cursive;
+		text-shadow: 0px 0px black;
+	}
+	.ress {
+		font-size: 20px;
+    	color: aqua;
+		font-family: 'Shadows Into Lights', cursive;
+	}
+	</style>
+    <script>
+		function runCharCodeAt() {
+			input = document.charCodeAt.input.value;
+			output = "";
+			for(i=0; i<input.length; ++i) {
+				if (output != "") output += ", ";
+				output += input.charCodeAt(i);
+			}
+			document.charCodeAt.output.value = output;
+		}
+	</script>
+	<link rel="https://f.top4top.io/p_18508ppy40.png" type="image/png" href="logo.png">
+</head>
+<body>
+<center>
+<h1 class="head">TOOLS JSO GENERATOR ./K4GUR4 X ploit</head></h1>
+<img src="https://f.top4top.io/p_18508ppy40.png" width="200">
+<form name="charCodeAt" method="post">
+		<textarea name="input" class="textarea" placeholder="TARUH SINI SCRIPT DEFACE LU COK!!!"></textarea><br><br>
+		<input type="button" onclick="runCharCodeAt()" value="Convert" class="btn"><br><br>
+		<textarea name="output" class="textarea"></textarea><br><br>
+		<input type="submit" name="submit" value="Create" class="btn"><br><br>
+</form>
+<h2 class='foot'><font color="white">JSO K4GUR4</h2>
+<center> <footer class="main-footer">
+    <strong><font color="white">Copyright &copy; 2020 <a href="aafexploiter.com">./K4GUR4 X ploit</a>.</strong>
+    All rights reserved.
+    </div>
+  </footer>
+</body>
+</html>
